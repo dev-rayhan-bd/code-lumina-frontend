ef6cb65e3abe22285d39168df798f6d14de06aca
rayhan108 <raihanshorker56@gmail.com>
2026-02-27 15:12:38 +0600

    modify next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 // next.config.ts
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -7,6 +9,17 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
       },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+
+        source: '/api/v1/:path*',
+
+        destination: 'https://ai-code-reviewer-lake-zeta.vercel.app/api/v1/:path*', 
+      },
+    ];
   },
 };
 
